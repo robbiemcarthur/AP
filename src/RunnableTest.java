9d6a49d0064444b785345ca80acc4d0faeb6710d
Robbie McArthur <2098323m@student.gla.ac.uk>
2018-03-04 14:26:09 +0000

    basic threading
    example of implementing runnable in a class then testing via main class

---
+++ src/RunnableTest.java
@@ -0,0 +1,16 @@
+
+public class RunnableTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int nThreads = 2;
+		Thread[] threads = new Thread[nThreads];
+		for (int i = 0; i<nThreads;i++)
+		{
+		PointlessPrint p = new PointlessPrint("I am thread " + (i+1), 10);
+		threads[i] = new Thread(p);
+		threads[i].start();
+		}
+	}
+
+}
